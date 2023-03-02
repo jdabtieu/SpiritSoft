ac2912b3b95084378029e91f1a0f9322cb219556
Jonathan Wu <jonathan.wu3@outlook.com>
2023-03-01 23:27:17 -0500

    Add help page

--- main/urls.py
+++ main/urls.py
@@ -9,11 +9,17 @@ def homepage(request):
     else:
         return redirect('/admin/')
 
+
+def helppage(request):
+    """Display help page"""
+    return render(request, 'help.html')
+
 def iframed(request):
     """IFrame view for embedded (desktop) app"""
     return render(request, 'iframed.html')
 
 urlpatterns = [
     path('', homepage, name='root_redirect'),
+    path('help/', helppage, name='help_page'),
     path('iframe/', iframed, name='iframed_container'),
 ]
